529baf1b8609572b79b18378da8fd5a18364f58d
WissemChihaoui <chihahoui10@gmail.com>
2023-05-11 17:09:13 +0100

    Update User

--- Front/src/pages/ListWanted.js
+++ Front/src/pages/ListWanted.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import NavBar from '../comp/NavBar'
-import AddUser from '../comp/AddUser'
-import UpdateUser from './UpdateUser'
+
 
 const ListWanted = () => {
   return (
     <div className='container-page'>
     <NavBar selected="listwanted" />
     <div>ListWanted</div>
-    <AddUser />
+    
     </div>
     
   )
